122e60b2d3a652af355c1e0e648196d57a05212b
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-04 21:29:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,8 +82,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
     end 
-  end 
-  holiday.flatten.compact 
+  end.flatten.compact 
 end
 
 

0a92e5f239e66c4b5f01da22d0f5e6d004cd4fae
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-04 21:31:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,14 +86,14 @@ def all_holidays_with_bbq(holiday_hash)
   end.flatten.compact 
 end
 
-holiday_hash.map do |season, holidays|
-    holidays.map do |holiday, items|
-      if items.include?("BBQ")
-        holiday
-      end
-    end
-  end.flatten.compact
-end
+# holiday_hash.map do |season, holidays|
+#     holidays.map do |holiday, items|
+#       if items.include?("BBQ")
+#         holiday
+#       end
+#     end
+#   end.flatten.compact
+# end
 
 
 

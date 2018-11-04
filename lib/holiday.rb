a79cd30eca7c1347a19f54d59539af33a99997b5
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-04 21:27:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,10 +76,11 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holiday|
-    holiday.collect 
+    holiday.collect do |holiday, item|
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+    end 
+  end 
 end
 
 

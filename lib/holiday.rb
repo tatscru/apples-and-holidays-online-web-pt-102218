ebd014c86d3802226fdb75a28f93065c0c16d8f5
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-04 20:42:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  second_supply_for_fourth_of_july(holiday_hash)
+  second_supply_for_fourth_of_july(holiday_supplies)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)

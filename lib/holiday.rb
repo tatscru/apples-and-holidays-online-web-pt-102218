fba25f1cd75edafd48160eef8bd787b6bae7cebc
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-04 21:11:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,9 +42,10 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
   # holiday_hash[:season][:name_of_season]= supply_hash
  
-  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)

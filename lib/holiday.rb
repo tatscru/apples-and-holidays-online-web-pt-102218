dd681270ba650669c034fc5f7e4e4435b8aac1d2
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-04 20:51:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the-- .each to iterate over both
   # Christmas AND the New Year's arrays
-
+  end 
 end
 
 

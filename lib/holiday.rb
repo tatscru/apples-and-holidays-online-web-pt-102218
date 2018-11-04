36347decedd0ca7091477f7548f92650ba130398
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-04 21:22:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,6 +70,7 @@ def all_supplies_in_holidays(holiday_hash)
     # you will first have to capitalize the first letter of each season 
     holiday.each do |holiday, supplies|
     puts "#{holiday.to_s.split("_").collect{|i| i.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    end 
   end
 end
 
